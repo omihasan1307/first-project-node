c76c5847262a6e8bfa91703f049c3589faaf04fe
omi hasan <omihasan1307@gmail.com>
2024-06-20 11:33:46 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/config/modules/student/student.route';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/v1/students/', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 10;
